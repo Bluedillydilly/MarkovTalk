9ce5c790156a307497b83ff46744f38898a62318
Bluedillydilly <dylancuprewich@gmail.com>
2021-11-02 12:02:34 -0400

    Better func docstrings. normalize done.

--- TransitionMatrix.py
+++ TransitionMatrix.py
@@ -1,6 +1,7 @@
 from numpy import zeros, sum
 from dataclasses import dataclass
 from typing import List, Tuple, TypedDict, NamedTuple, Dict
+from functools import reduce
 
 @dataclass
 class TransitionMatrix:
@@ -12,15 +13,16 @@ class TransitionMatrix:
     matrix: Dict[str,Dict[str, float]] = {}
 
     def initializeMatrix(self) -> None:
-        """
-            Initializes a N x N matrix dependent on words list.
+        """Initializes a N x N matrix dependent on words list.
         """
         self.matrix = zeros((self.word_count, self.word_count))
 
 
     def addWord(self, word: str) -> None:
-        """
-            A new word to add as a key to the transition matrix.
+        """A new word to add as a key to the transition matrix.
+
+            Param:
+                word: potential new word to add
         """
         if word.lower() not in self.words:
             # add to word list
@@ -32,8 +34,10 @@ class TransitionMatrix:
 
 
     def getWords(self) -> List[str]:
-        """
-            Gets the list of words in matrix
+        """Gets the list of words in matrix
+
+            Return:
+                list of all words in the transition matrix.
         """
         return self.words
 
@@ -42,6 +46,13 @@ class TransitionMatrix:
         """
             Get the number of BEFORE - AFTER word pair frequency.
             After normalizing, gets the density of the pair.
+
+            Param:
+                before: first word in pair
+                after: second word in pair
+
+            Return:
+                freqency/density of BEFORE - AFTER occurrence
         """
         return self.matrix[before][after]
 
@@ -49,28 +60,38 @@ class TransitionMatrix:
     def getRow(self, word) -> Dict[str, float]:
         """
             Gets after words of a before word.
+
+            Return:
+                dict of after words to frequency/density
         """
         return self.matrix[word]
 
     def addPairs(self, pairs: List[(str, str)]) -> None:
         """
             Add multiple BEFORE - AFTER word pairs.
+
+            Param:
+                pairs: list of BEFORE - AFTER pair to add
         """
         for (before, after) in pairs:
-            pass
+           self.matrix[before][after] = self.matrix[before].get(after, 0)
 
-    def addPair(self, before, after):
+    def addPair(self, before, after) -> None:
         """
             Adds a WORD - WORD pair to the matrix.
             Increases the element to show the before is followed by after.
 
         """
-        self.matrix[self.keys[before]][self.keys[after]] += 1
+        self.matrix[before][after] = self.matrix[before].get(after, 0)
 
 
-    def normalize(self):
+    def normalize(self) -> None:
         """
-            turns the raw counts into ratios that sum to 1 for the row
+            turns the AFTER word frequencies into densities that sum to 1 for the AFTER row. 
         """
-        for row in range(self.getKeyCount()):
-            self.matrix[row] /= sum(self.matrix[row])
+        for (before,v) in self.matrix.items():
+            # number of total pairs for BEFORE 
+            rowCount: int = reduce( sum, v.values() )
+            # update counts
+            for after in v.keys():
+                self.matrix[before][after] /= rowCount
